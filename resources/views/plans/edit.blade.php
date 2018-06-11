2021fc3b74c435874c7b695a2bf04dba307f081a
Bryan Monzon <monzonb@gmail.com>
2018-06-11 13:52:29 -0700

    Loop through eager loaded relationships

--- resources/views/plans/edit.blade.php
+++ resources/views/plans/edit.blade.php
@@ -2,12 +2,27 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mb-4">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{$plan->name}}</div>
                 <div class="card-body">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos molestias, expedita odio culpa delectus voluptatum consequatur velit, vero dolorem provident error animi reprehenderit aspernatur unde sint accusantium veniam nemo sit.
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="mb-2 text-right">
+                <a href="#" class="btn btn-primary">Add Projects to this Plan</a>
+            </div>
+            <div class="card">
+                <div class="card-header">Projects</div>
+                <div class="list-group">
+                    @foreach( $plan->projects as $project)
+                        <a href="{{route('plan.projects.show', [$plan, $project])}}" class="list-group-item list-group-item-action">{{$project->name}}</a>
+                    @endforeach
                 </div>
             </div>
         </div>
