522d1479acb45914f3eadbef715fe65b5cb9d5fc
Bryan Monzon <monzonb@gmail.com>
2018-06-11 16:21:57 -0700

    Recently compiled stuff

--- public/js/app.js
+++ public/js/app.js
@@ -47686,17 +47686,8 @@ module.exports = Component.exports
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__ProjectItem__ = __webpack_require__(57);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__ProjectItem___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__ProjectItem__);
 //
 //
 //
@@ -47706,7 +47697,12 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 
+
 /* harmony default export */ __webpack_exports__["default"] = ({
+    props: ['endpoint'],
+    components: {
+        ProjectItem: __WEBPACK_IMPORTED_MODULE_0__ProjectItem___default.a
+    },
     data: function data() {
         return {
             projects: []
@@ -47725,7 +47721,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
         fetchProjects: function fetchProjects() {
             var _this = this;
 
-            axios.get('/projects').then(function (res) {
+            axios.get(this.endpoint).then(function (res) {
+                console.log(res);
                 _this.projects = res.data;
             }).catch(function (err) {
                 console.log(err);
@@ -47747,35 +47744,10 @@ var render = function() {
       "div",
       { staticClass: "list-group" },
       _vm._l(_vm.projects, function(project) {
-        return _c(
-          "a",
-          {
-            key: project.id,
-            staticClass: "list-group-item list-group-item-action d-flex",
-            attrs: { href: "/projects/" + project.id }
-          },
-          [
-            _c("span", { staticClass: "mr-auto p-2" }, [
-              _vm._v(
-                "\n                " + _vm._s(project.name) + "\n            "
-              )
-            ]),
-            _vm._v(" "),
-            _c("span", { staticClass: "p-2" }, [
-              project.completed
-                ? _c(
-                    "span",
-                    { staticClass: "badge badge-pill badge-secondary" },
-                    [_vm._v("Completed")]
-                  )
-                : _c(
-                    "span",
-                    { staticClass: "badge badge-pill badge-success" },
-                    [_vm._v("In-Progress")]
-                  )
-            ])
-          ]
-        )
+        return _c("project-item", {
+          key: project.id,
+          attrs: { project: project }
+        })
       })
     )
   ])
@@ -47980,6 +47952,123 @@ if (false) {
   module.hot.accept()
   if (module.hot.data) {
     require("vue-hot-reload-api")      .rerender("data-v-1ae7cde7", module.exports)
+  }
+}
+
+/***/ }),
+/* 57 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var disposed = false
+var normalizeComponent = __webpack_require__(40)
+/* script */
+var __vue_script__ = __webpack_require__(58)
+/* template */
+var __vue_template__ = __webpack_require__(59)
+/* template functional */
+var __vue_template_functional__ = false
+/* styles */
+var __vue_styles__ = null
+/* scopeId */
+var __vue_scopeId__ = null
+/* moduleIdentifier (server only) */
+var __vue_module_identifier__ = null
+var Component = normalizeComponent(
+  __vue_script__,
+  __vue_template__,
+  __vue_template_functional__,
+  __vue_styles__,
+  __vue_scopeId__,
+  __vue_module_identifier__
+)
+Component.options.__file = "resources/assets/js/components/projects/ProjectItem.vue"
+
+/* hot reload */
+if (false) {(function () {
+  var hotAPI = require("vue-hot-reload-api")
+  hotAPI.install(require("vue"), false)
+  if (!hotAPI.compatible) return
+  module.hot.accept()
+  if (!module.hot.data) {
+    hotAPI.createRecord("data-v-cbf812a4", Component.options)
+  } else {
+    hotAPI.reload("data-v-cbf812a4", Component.options)
+  }
+  module.hot.dispose(function (data) {
+    disposed = true
+  })
+})()}
+
+module.exports = Component.exports
+
+
+/***/ }),
+/* 58 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+    props: ['project']
+});
+
+/***/ }),
+/* 59 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("div", [
+    _c(
+      "a",
+      {
+        staticClass: "list-group-item list-group-item-action d-flex",
+        attrs: { href: "/projects/" + _vm.project.id }
+      },
+      [
+        _c("span", { staticClass: "mr-auto p-2" }, [
+          _vm._v("\n            " + _vm._s(_vm.project.name) + "\n        ")
+        ]),
+        _vm._v(" "),
+        _c("span", { staticClass: "p-2" }, [
+          _vm.project.completed
+            ? _c("span", { staticClass: "badge badge-pill badge-secondary" }, [
+                _vm._v("Completed")
+              ])
+            : _c("span", { staticClass: "badge badge-pill badge-success" }, [
+                _vm._v("In-Progress")
+              ])
+        ])
+      ]
+    )
+  ])
+}
+var staticRenderFns = []
+render._withStripped = true
+module.exports = { render: render, staticRenderFns: staticRenderFns }
+if (false) {
+  module.hot.accept()
+  if (module.hot.data) {
+    require("vue-hot-reload-api")      .rerender("data-v-cbf812a4", module.exports)
   }
 }
 
