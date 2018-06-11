c754947095ed1e61b4db3ba8376ed6f0b56359b7
Bryan Monzon <monzonb@gmail.com>
2018-06-11 10:44:57 -0700

    Star the AddPlanTest

---
+++ tests/Feature/Plans/AddPlanTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\Plans;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AddPlanTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test */
+    public function a_user_can_add_a_plan()
+    {
+        $this->signIn();
+
+        $this->post('plans', [
+            'name' => 'Week of June 11 Plan'
+        ]);
+
+        $this->assertDatabaseHas('plans', [
+            'name' => 'Week of June 11 Plan'
+        ]);
+    }
+
+    /** @test */
+    public function a_plan_must_have_a_name()
+    {
+        $this->signIn();
+
+        $response = $this->post('plans', [
+            'name' => ''
+        ]);
+
+        $response->assertSessionHasErrors('name');
+    }
+}
