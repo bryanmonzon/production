3e39216a2371bbacdf79089fd76aae01d84cfc44
Bryan Monzon <monzonb@gmail.com>
2018-06-11 15:08:48 -0700

    PlansList listens for new plans now

--- resources/assets/js/components/plans/PlansList.vue
+++ resources/assets/js/components/plans/PlansList.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <div class="list-group">
-            <a href="#" 
+            <a :href='"/plans/"+plan.id'
                 class="list-group-item list-group-item-action d-flex"
                 v-for="plan in plans" :key="plan.id"
             >
                 <span class="mr-auto p-2">
-                    <a :href='"/plans/"+plan.id'>{{plan.name}}</a>
+                    {{plan.name}}
                 </span>
                 <span class="p-2 text-secondary">
                     {{ plan.projects.length}} projects
@@ -27,16 +27,23 @@
                 plans: []
             }
         },
-        mounted() {
+        created() {
+            let self = this
             this.fetchPlans()
+
+            Bus.$on('plan:added', function() {
+                self.fetchPlans()
+            });
         },
         methods: {
             fetchPlans() {
                 axios.get('/plans')
                     .then( res => {
-                        console.log(res.data);
                         this.plans = res.data
-                    });
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
             }
         }
     }
