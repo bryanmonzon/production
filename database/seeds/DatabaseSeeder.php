77759308b76fcbffe390845ae6bec34cc1151166
Bryan Monzon <monzonb@gmail.com>
2018-06-11 09:24:59 -0700

    yarn and font update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(App\User::class, 1)->create([
+            'name' => 'Bryan Monzon',
+            'email' => 'bryanm@fiftyandfifty.org',
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
+        ]);
     }
 }
