77f1a4f4f92b7b97d854b716267940d2a6a38d98
Bryan Monzon <monzonb@gmail.com>
2018-06-12 12:15:02 -0700

    WIP markup for ability to add plans to projects

--- resources/views/plans/edit.blade.php
+++ resources/views/plans/edit.blade.php
@@ -15,7 +15,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="mb-2 text-right">
-                <a href="#" class="btn btn-primary">Add Projects to this Plan</a>
+                <button class="btn btn-primary" data-toggle="modal" data-target="#projectPicker">Add Projects to this Plan</button>
             </div>
             <div class="card">
                 <div class="card-header">Projects</div>
@@ -24,6 +24,38 @@
         </div>
     </div>
 </div>
+
+<div class="modal" id="projectPicker" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <form action="/wherevs" method="POST">
+        @csrf
+        @method('PATCH')
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Add Projects to this Plan</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+                @foreach($projects as $project)
+                <div class="form-check">
+                  <input class="form-check-input" name="planProjects[]" type="checkbox" value="{{$project->id}}" id="project-{{$project->id}}">
+                  <label class="form-check-label" for="project-{{$project->id}}">
+                    {{$project->name}}
+                  </label>
+                </div>
+                @endforeach
+          </div>
+          <div class="modal-footer">
+            <button type="submit" class="btn btn-primary">Save changes</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+    </form>
+  </div>
+</div>
+
 <div class="fixed-bottom production-footer bg-light border-top py-2">
     <div class="container d-flex justify-content-between">
         <form action="{{route('plans.delete', $plan)}}" method="POST">
