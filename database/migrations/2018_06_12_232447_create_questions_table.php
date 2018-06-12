fc3937ede341fbe7fea8739f3142acef6c22aff3
Bryan Monzon <monzonb@gmail.com>
2018-06-12 16:48:46 -0700

    Add migration for questions

---
+++ database/migrations/2018_06_12_232447_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('project_id')->index();
+            $table->unsignedInteger('user_id')->index();
+            $table->text('question');
+            $table->boolean('resolved')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
