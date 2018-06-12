f8e0fc98613910b215a3793e668d6e9b5bc21081
Bryan Monzon <monzonb@gmail.com>
2018-06-11 21:16:47 -0700

    remove wrapper div

--- resources/assets/js/components/projects/ProjectItem.vue
+++ resources/assets/js/components/projects/ProjectItem.vue
@@ -1,17 +1,15 @@
 <template>
-    <div>
-        <a :href='"/projects/"+project.id'
-            class="list-group-item list-group-item-action d-flex"
-        >
-            <span class="mr-auto p-2">
-                {{project.name}}
-            </span>
-            <span class="p-2">
-                <span class="badge badge-pill badge-secondary" v-if="project.completed">Completed</span>
-                <span class="badge badge-pill badge-success" v-else>In-Progress</span>
-            </span>
-        </a>
-    </div>
+    <a :href='"/projects/"+project.id'
+        class="list-group-item list-group-item-action d-flex"
+    >
+        <span class="mr-auto p-2">
+            {{project.name}}
+        </span>
+        <span class="p-2">
+            <span class="badge badge-pill badge-secondary" v-if="project.completed">Completed</span>
+            <span class="badge badge-pill badge-success" v-else>In-Progress</span>
+        </span>
+    </a>
 </template>
 
 <script>
