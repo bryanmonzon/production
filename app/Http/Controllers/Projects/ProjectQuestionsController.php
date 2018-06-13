309f53f144a04f210b445223d7579119e9462735
Bryan Monzon <monzonb@gmail.com>
2018-06-12 20:25:11 -0700

    Eager load user with questions

--- app/Http/Controllers/Projects/ProjectQuestionsController.php
+++ app/Http/Controllers/Projects/ProjectQuestionsController.php
@@ -10,8 +10,7 @@ class ProjectQuestionsController extends Controller
 {
     public function index(Project $project)
     {
-        //TODO: include users in this query
-        return response()->json($project->questions()->orderBy('created_at', 'desc')->get());    
+        return response()->json($project->questions()->with('user')->orderBy('created_at', 'desc')->get());    
     }
 
     public function store(Project $project)
