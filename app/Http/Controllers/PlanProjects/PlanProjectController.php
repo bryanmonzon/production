058836a8d18adc074a94fa94e36b1dfd8ac68900
Bryan Monzon <monzonb@gmail.com>
2018-06-13 11:22:47 -0700

    Switch the sync method to store and add delete method

--- app/Http/Controllers/PlanProjects/PlanProjectController.php
+++ app/Http/Controllers/PlanProjects/PlanProjectController.php
@@ -22,4 +22,24 @@ class PlanProjectController extends Controller
             'project' => $project,
         ]);
     }
+
+    public function store(Plan $plan)
+    {
+        
+        foreach( request('projects') as $projectId ) {
+           
+            if( !$plan->projects->contains( $projectId ) ) {
+                $plan->projects()->attach($projectId);
+            }
+        }
+
+        return response()->json($plan->projects()->get(), 201);        
+    }
+
+    public function delete(Plan $plan, Project $project)
+    {
+        $plan->projects()->detach($project->id);
+
+        return response(204);
+    }
 }
