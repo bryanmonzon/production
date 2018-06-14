4dce2659baff99ae71b5ad38f220aa8e1c890b7b
Bryan Monzon <monzonb@gmail.com>
2018-06-14 16:19:10 -0700

    QuestionCommentsForm component created

---
+++ resources/assets/js/components/projects/questions/QuestionCommentForm.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="comment-form pb-3">
+      <input type="text" class="form-control rounded" placeholder="Write a comment" @keydown.enter="submitComment" v-model="form.body">
+    </div>
+</template>
+<script>
+    export default {
+        props: ['question'],
+        data() {
+            return {
+                form: {
+                    body: ''
+                }
+            }
+        },
+        methods: {
+            submitComment() {
+                axios.post(`/questions/${this.question.id}/comments`, this.form)
+                    .then(res => {
+                        this.form.body = ''
+                        Bus.$emit('comment:added', res.data)
+                    })
+            }
+        }
+    }
+</script>
