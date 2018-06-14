d7b8f00aff5b8d9bb53c69f6d2469866223239e6
Bryan Monzon <monzonb@gmail.com>
2018-06-13 17:00:33 -0700

    revert questionitem temp

---
+++ resources/assets/js/components/projects/questions/QuestionItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="list-group-item pt-4">
+      <div class="w-100 lead">
+          {{question.question}}
+      </div>
+      <div class="mw-100 mt-2 pt-2 text-muted font-italic d-flex justify-content-between">
+          <span>asked by {{ question.user.name }} - {{ question.created_at | datetime }}</span>
+          <span>
+              <label style="margin-bottom:0px;" :for="'question-'+question.id" class="text-muted text-sm-left mr-1 d-flex justify-content-end align-items-center">
+                  <span class="mr-2">Resolve</span>
+                  <input type="checkbox" :id="'question-'+question.id">
+              </label>
+          </span>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['question']
+    }
+</script>
