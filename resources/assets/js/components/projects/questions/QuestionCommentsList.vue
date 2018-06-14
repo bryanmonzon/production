92316eb70927c1f57066b2d3b56952bed2969eca
Bryan Monzon <monzonb@gmail.com>
2018-06-14 16:18:36 -0700

    Add QuestionCommentsList component

---
+++ resources/assets/js/components/projects/questions/QuestionCommentsList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <div class="w-100 pt-1 text-muted">
+        <span>{{comments.length}} Comments</span>
+      </div>
+      
+      <div class="list-group question-comments text-muted">
+        <div class="list-group-item" :class="{ 'bg-light': question.resolved }" v-for="comment in comments" :key="comment.id">
+          <div class="d-flex flex-column">
+            <span><strong>{{comment.user.name}}</strong> {{comment.body}}</span>
+            <span style="font-size:.75rem;">{{comment.user.created_at | datetime}}</span>
+          </div>
+        </div>
+      </div>
+      
+      <question-comment-form :question="question" />
+    </div>
+</template>
+<script>
+    import QuestionCommentForm from './QuestionCommentForm'
+
+    export default {
+        props: ['comments', 'question'],
+        components: {
+            QuestionCommentForm
+        }
+    }
+</script>
--- resources/assets/js/components/projects/questions/QuestionItem.vue
+++ resources/assets/js/components/projects/questions/QuestionItem.vue
@@ -1,36 +1,37 @@
 <template>
-    <div class="list-group-item pt-4" :class="{ 'unresolved': !question.resolved, 'bg-light': question.resolved }">
-      <div class="w-100 lead">
-          <span :class="{'resolved': question.resolved}">{{question.question}}</span>
-      </div>
-      <div class="mw-100 mt-2 pt-2 text-muted d-flex justify-content-between align-items-center">
-          <span class="d-flex flex-row align-items-center">
+    <div class="list-group-item" :class="{ 'unresolved': !question.resolved, 'bg-light': question.resolved }">
+      <div class="mw-100 text-muted d-flex justify-content-between align-items-center">
+          <span class="d-flex flex-row align-items-center author-wrap">
             <img :src="question.user.avatar" style="height:30px; width:30px; box-shadow: 0 2px 4px 0 rgba(0,0,0,0.10);" class="rounded-circle mr-2" /> 
             <span class="d-flex flex-column">
-              <span>{{ question.user.name }}</span>
+              <a href="#"><strong>{{ question.user.name }}</strong></a>
               <span style="font-size:.75rem;" class="font-italic">{{ question.created_at | datetime }}</span>
             </span>
           </span>
           <resolve-question :question="question" />
       </div>
+      <div class="w-100 lead py-4">
+          <span :class="{'resolved': question.resolved}">{{question.question}}</span>
+      </div>
+      <question-comments-list :comments="question.comments" :question="question" />
     </div>
 </template>
 
 <script>
     import ResolveQuestion from './ResolveQuestion'
+    import QuestionCommentsList from './QuestionCommentsList'
     export default {
         props: ['question'],
         components: {
-          ResolveQuestion
+          ResolveQuestion,
+          QuestionCommentsList
         }, 
         created() {
           let self = this
 
           Bus.$on('question:resolved', function(data) {
-
             if( data.id == self.question.id ) {
               self.question.resolved = !self.question.resolved
-
             }
           })
 
@@ -40,11 +41,20 @@
 </script>
 
 <style>
+  .author-wrap {
+    line-height: 1rem;
+  }
   .resolved {
     text-decoration:line-through;
     color: #9da6af !important
   }
   .unresolved {
     border-top:3px solid green;
+  }
+  .question-comments {
+    margin-top:5px;
+  }
+  .comment-form {
+    margin-top:10px;
   }
 </style>
