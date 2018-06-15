efcdb6becb66db832f1e9a8f9ebbb023e98ddb9f
Bryan Monzon <monzonb@gmail.com>
2018-06-15 16:09:49 -0700

    add project concern controller methods

---
+++ app/Http/Controllers/Projects/ProjectConcernsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Projects;
+
+use App\Project;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProjectConcernsController extends Controller
+{
+    public function index(Project $project)
+    {
+        return response()->json($project->concerns()->with('user')->get(), 200);
+    }
+
+    public function store(Project $project)
+    {
+        request()->validate([
+            'body' => 'required'
+        ]);
+        
+        $concern = $question->comments()->make([
+            'body' => request('body')
+        ]);
+
+        $concern = request()->user()->concernss()->save($concern);
+
+        return response()->json($concern->load('user'), 201);
+    }
+}
