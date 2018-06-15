78613efba1e7707dc4eca244d818017bf9eed04c
Bryan Monzon <monzonb@gmail.com>
2018-06-15 11:56:20 -0700

    pass the comment into the bus when deleting

--- resources/assets/js/components/projects/questions/QuestionCommentItem.vue
+++ resources/assets/js/components/projects/questions/QuestionCommentItem.vue
@@ -2,7 +2,7 @@
     <div class="list-group-item" :class="{ 'bg-light': question.resolved }">
       <div class="d-flex flex-column">
         <span><strong>{{comment.user.name}}</strong> {{comment.body}}</span>
-        <span style="font-size:.75rem;">{{comment.created_at | datetime}} <span v-if="ownsComment">• <a href="#" class="text-muted" @click.prevent="deleteComment">Delete</a></span></span>
+        <span style="font-size:.75rem;">{{comment.created_at | datetime}} <span v-if="ownsComment">• <a href="#" class="text-muted" @click.prevent="deleteComment(comment)">Delete</a></span></span>
       </div>
     </div>
 </template>
@@ -13,10 +13,10 @@
             ownsComment() {
                 return this.comment.id === user.id
             },
-            deleteComment() {
-                axios.delete(`/comments/${this.comment.id}`)
+            deleteComment(comment) {
+                axios.delete(`/comments/${comment.id}`)
                     .then(res => {
-                        Bus.$emit('comment:deleted')
+                        Bus.$emit('comment:deleted', comment)
                     })
             }
         }
