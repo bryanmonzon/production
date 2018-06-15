ede5d5a1112168705e9ee02673c75402204feb7a
Bryan Monzon <monzonb@gmail.com>
2018-06-15 15:08:48 -0700

    Add the ux logic for editing a question

--- resources/assets/js/components/projects/questions/QuestionItem.vue
+++ resources/assets/js/components/projects/questions/QuestionItem.vue
@@ -5,13 +5,18 @@
             <img :src="question.user.avatar" style="height:30px; width:30px; box-shadow: 0 2px 4px 0 rgba(0,0,0,0.10);" class="rounded-circle mr-2" /> 
             <span class="d-flex flex-column">
               <a href="#"><strong>{{ question.user.name }}</strong></a>
-              <span style="font-size:.75rem;" class="font-italic">{{ question.created_at | datetime }} <span v-if="ownsQuestion">• <a href="#" @click.prevent="deleteQuestion(question)" class="text-muted">Delete Question</a></span></span>
+              <span style="font-size:.75rem;" class="font-italic">{{ question.created_at | datetime }} <span v-if="ownsQuestion(question)">• <a href="#" @click.prevent="deleteQuestion(question)" class="text-muted">Delete</a> or <a href="#" class="text-muted" @click.prevent="toggleEdit">Edit</a> Question</span></span>
             </span>
           </span>
           <resolve-question :question="question" />
       </div>
       <div class="w-100 lead py-4">
-          <span :class="{'resolved': question.resolved}">{{question.question}}</span>
+          <span :class="{'resolved': question.resolved}" @click.prevent="editing = true" v-if="!editing">{{question.question}}</span>
+          <div v-else>
+            <textarea class="form-control" @keydown.enter="updateQuestion" v-model="form.question">{{question.question}}</textarea>
+            <span style="font-size:.75rem;" class="font-italic"><a href="#" class="text-muted" @click.prevent="editing = false">Cancel</a></span>
+          </div>
+
       </div>
       <question-comments-list :endpoint="`/questions/${question.id}/comments`" :comments="question.comments" :question="question" />
     </div>
@@ -26,9 +31,17 @@
           ResolveQuestion,
           QuestionCommentsList
         },
+        data() {
+          return {
+            form: {
+              question: this.question.question
+            },
+            editing: false
+          }
+        },
         created() {
           let self = this
-
+          console.log(this.question)
           Bus.$on('question:resolved', function(data) {
             if( data.id == self.question.id ) {
               self.question.resolved = !self.question.resolved
@@ -36,14 +49,24 @@
           })
         },
         methods: {
-          ownsQuestion() {
-            return user.id === this.question.user_id
+          ownsQuestion(question) {
+            return user.id === question.user.id
           },
           deleteQuestion(question) {
             axios.delete(`/questions/${question.id}`)
               .then( res => {
                 Bus.$emit('question:deleted', question);
               })
+          },
+          updateQuestion() {
+            axios.patch(`/questions/${this.question.id}`, this.form)
+              .then(res => {
+                this.question.question = res.data.question
+                this.editing = false
+              })
+          },
+          toggleEdit() {
+            return this.editing = !this.editing
           }
         }
     }
