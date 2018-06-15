b1b7bef377df620ed656f0514c9c71b415d24a1f
Bryan Monzon <monzonb@gmail.com>
2018-06-15 16:52:57 -0700

    and btn-sm to button

--- resources/assets/js/components/plans/projects/AddProjectsPlanButton.vue
+++ resources/assets/js/components/plans/projects/AddProjectsPlanButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="btn btn-primary" @click="showProjectsPlanModal">
+    <button class="btn btn-sm btn-primary" @click="showProjectsPlanModal">
         Add Projects to this Plan
     </button>
 </template>
