1bcf80fa3ac0d62ca3d3eb72b0104036747d7300
Bryan Monzon <monzonb@gmail.com>
2018-06-15 16:53:29 -0700

    adjust plans/project edit template a bit

--- resources/views/plans/projects/edit.blade.php
+++ resources/views/plans/projects/edit.blade.php
@@ -5,6 +5,19 @@
     <div class="row justify-content-center mb-4">
         <div class="col-md-8">
             <div class="card">
+                <div class="card-header">Concerns</div>
+                <div class="card-body">
+                    <new-concern endpoint="{{route('projects.concerns.store', $project) }}" />
+                </div>
+
+                <concern-list endpoint="{{ route('projects.concerns.index', $project) }}" />
+            </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center mb-4">
+        <div class="col-md-8">
+            <div class="card">
                 <div class="card-header">{{$plan->name}} - {{ $project->name}}</div>
                 <div class="card-body">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos molestias, expedita odio culpa delectus voluptatum consequatur velit, vero dolorem provident error animi reprehenderit aspernatur unde sint accusantium veniam nemo sit.
@@ -16,10 +29,13 @@
     <div class="row justify-content-center mb-4">
         <div class="col-md-8">
             <div class="mb-2 text-right">
-                <add-users-project-button></add-users-project-button>
+                
             </div>
             <div class="card">
-                <div class="card-header">Users</div>
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <span>Users</span>
+                    <add-users-project-button></add-users-project-button>
+                </div>
                 <users-project-list 
                     project="{{ $project }}" 
                     endpoint="{{ route('projects.users', $project) }}" 
