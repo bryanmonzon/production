9a2d84961d38110699386efed9321079cc7c3aa5
Bryan Monzon <monzonb@gmail.com>
2018-06-15 20:18:18 -0700

    add styles for resolved concerns

--- resources/assets/js/components/projects/concerns/ConcernItem.vue
+++ resources/assets/js/components/projects/concerns/ConcernItem.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="list-group-item d-flex justify-content-between align-items-center" :class="classObject">
         <span class="d-flex flex-wrap align-items-center">
-            <div class="mr-2">{{ concern.body }}</div>
-            <span class="concern-date text-muted">{{concern.created_at | date }} by {{concern.user.name}}</span>
+            <div class="mr-2 concern-body">{{ concern.body }}</div>
+            <span class="concern-date text-muted concern-author">{{concern.created_at | date }} by {{concern.user.name}}</span>
         </span>
         <span>
-            <button class="btn btn-sm btn-outline-secondary">Mark as Resolved</button>
+            <button class="btn btn-sm btn-outline-secondary" v-if="!concern.resolved"><i class="far fa-circle"></i> Resolve</button>
+            <button class="btn btn-sm btn-success" v-else><i class="far fa-check-circle"></i> Resolved</button>
         </span>
     </div>
 </template>
@@ -20,6 +21,7 @@
                     'priority-3': this.concern.priority === 3,
                     'priority-2': this.concern.priority === 2,
                     'priority-1': this.concern.priority === 1,
+                    'concern-resolved': this.concern.resolved === 1,
                 }
             }
         }
@@ -38,5 +40,9 @@
     }
     .priority-1 {
         border-left: 3px solid #dc3545;
+    }
+    .concern-resolved .concern-body,
+    .concern-resolved .concern-author {
+        opacity: .5;
     }
 </style>
