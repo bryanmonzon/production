4812855a89b11152e65449fdba95e0954bfd92a9
Bryan Monzon <monzonb@gmail.com>
2018-06-15 20:30:37 -0700

    update controller method

---
+++ app/Http/Controllers/Concerns/ConcernsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Concerns;
+
+use App\Concern;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ConcernsController extends Controller
+{
+    public function update(Concern $concern)
+    {
+        $concern->update(request()->all());
+
+        return response()->json($concern, 202);
+    }
+}
