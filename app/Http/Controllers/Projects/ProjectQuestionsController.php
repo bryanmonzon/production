8f44732cb7a3118775d0ecba194a74092625e720
Bryan Monzon <monzonb@gmail.com>
2018-06-18 13:55:52 -0700

    Add new delete method with project typehint so we can listen on sockets

--- app/Http/Controllers/Projects/ProjectQuestionsController.php
+++ app/Http/Controllers/Projects/ProjectQuestionsController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Projects;
 
 use App\Project;
+use App\Question;
 use Illuminate\Http\Request;
 use App\Events\QuestionWasCreated;
+use App\Events\QuestionWasDeleted;
 use App\Http\Controllers\Controller;
 
 class ProjectQuestionsController extends Controller
@@ -28,5 +30,15 @@ class ProjectQuestionsController extends Controller
         broadcast(new QuestionWasCreated($question->load('user')))->toOthers();
 
         return response()->json($question, 201);
+    }
+
+    public function delete(Project $project, Question $question)
+    {
+        $question->comments()->delete();
+        $question->delete();
+
+        broadcast(new QuestionWasDeleted($question->id, $project->id))->toOthers();
+
+        return response()->json(204);
     }
 }
