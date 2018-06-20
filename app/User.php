498bfc6fd49b2b7c82680f9fb7bcf414ffe06e1d
Bryan Monzon <monzonb@gmail.com>
2018-06-20 10:23:25 -0700

    Task <-> User relationships

--- app/User.php
+++ app/User.php
@@ -48,6 +48,11 @@ class User extends Authenticatable
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
     
     public function avatar()
     {
