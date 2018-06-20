7846302c35778006e64c500eea2740574cf1aa41
Bryan Monzon <monzonb@gmail.com>
2018-06-20 10:23:05 -0700

    Tasks migration

---
+++ database/migrations/2018_06_20_161140_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->index();
+            $table->boolean('completed')->default(false);
+            $table->text('body');
+            $table->dateTime('due_date');
+            $table->unsignedInteger('priority')->default(4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
