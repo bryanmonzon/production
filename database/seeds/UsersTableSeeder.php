18cf90fe12cb9fb748372ff735418201c6041429
Bryan Monzon <monzonb@gmail.com>
2018-06-21 11:30:14 -0700

    seed user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,6 +17,12 @@ class UsersTableSeeder extends Seeder
             'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
         ]);
 
+        factory(App\User::class)->create([
+            'name' => 'Sandy Monzon',
+            'email' => 'sjfowles@gmail.com',
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
+        ]);
+
         factory(App\User::class, 10)->create();
 
     }
